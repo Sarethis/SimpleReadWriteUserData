235e64b398156fcb87f454b9ebad6a1e30376c41
Sareth <mateusz.kiela@gmail.com>
2022-02-13 14:10:18 +0100

    Add project files.

---
+++ PracaDomowaTydz3_1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracaDomowaTydz3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, place;
+            int yearOfBirth, monthOfBirth, dayOfBirth;
+            int userChoice;
+            int i = 1;
+
+            Console.WriteLine("Cześć, poproszę Cię o podanie kilku informacji o sobie, a później sprawdzimy, czy dobrze zapamiętałem!");
+
+            try
+            {
+                do
+                {
+                    Console.WriteLine(User.GetCurrentDate());
+
+                    Console.WriteLine("Podaj swoje imie: ");
+                    name = Console.ReadLine();
+                    Console.WriteLine("A gdzie się urodziłeś?");
+                    place = Console.ReadLine();
+                    Console.WriteLine("Teraz podaj rok urodzenia: ");
+                    yearOfBirth = User.GetInputYear();
+                    Console.WriteLine("Teraz podaj miesiąc urodzenia: ");
+                    monthOfBirth = User.GetInputMonth();
+                    Console.WriteLine("Teraz podaj dzień urodzenia: ");
+                    dayOfBirth = User.GetInputDay(yearOfBirth, monthOfBirth);
+
+                    DateTime userDateOfBirth = new DateTime(yearOfBirth, monthOfBirth, dayOfBirth);
+                    Console.WriteLine(userDateOfBirth);
+
+                    User thisUser = new User(name, place, yearOfBirth, monthOfBirth, dayOfBirth);
+
+                    Console.WriteLine("Teraz sprawdźmy, czy dobrze zapamiętałem...");
+
+                    thisUser.ReadAllData();
+
+                    Console.WriteLine("Chcesz powtórzyć trening?\n1. Tak!\n2. Nie.");
+                    userChoice = User.GetInputData();
+
+                    if (userChoice == 1)
+                        i++;
+                }
+                while (userChoice != 2) ;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Error: {exception.Message}");
+            }
+        }
+    }
+}
---
+++ PracaDomowaTydz3_1/User.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracaDomowaTydz3_1
+{
+    internal class User
+    {
+        private string _name;
+        private string _placeOfLiving;
+        private int _yearOfBirth;
+        private int _monthOfBirth;
+        private int _dayOfBith;
+
+        static int CurrentYear = DateTime.Now.Year;
+        static int CurrentMonth = DateTime.Now.Month;
+        static int CurrentDay = DateTime.Now.Day;
+
+        public User(string name, string placeOfLiving, int yearOfBirth, int monthOfBirth, int dayOfBirth)
+        {
+            _name = name;
+            _placeOfLiving = placeOfLiving;
+            _yearOfBirth = yearOfBirth;
+            _monthOfBirth = monthOfBirth;
+            _dayOfBith = dayOfBirth;
+        }
+        public static int GetInputData()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int variable))
+                {
+                    Console.WriteLine("Podana wartość nie jest liczbą.\nPodaj poprawną wartość:");
+                    continue;
+                }
+                return variable;
+            }
+        }
+
+        public static int GetInputYear()
+        {
+            while (true)
+            { 
+                if (!int.TryParse(Console.ReadLine(), out int variable) || variable < 1902 || variable > DateTime.Now.Year)
+                {
+                    Console.WriteLine("Podana wartość nie jest rokiem między 1902 a 2022... Chyba nie masz więcej niż 120 lat oraz urodziłeś się już.\nPodaj poprawną wartość:");
+                    continue;
+                }
+                return variable; 
+            }
+        }
+        public static int GetInputMonth()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int variable) || variable < 1 || variable > 12)
+                {
+                    Console.WriteLine("Podana wartość nie jest numerem miesiąca z planety Ziemia.\nPodaj jeszcze raz poprawną wartość:");
+                    continue;
+                }
+                return variable;
+            }
+        }
+        public static int GetInputDay(int year, int month)
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int variable) || variable < 1 || variable > DateTime.DaysInMonth(year, month))
+                {
+                    Console.WriteLine("Podana wartość nie jest poprawną liczbą dni.\nPodaj poprawną wartość:");
+                    continue;
+                }
+                return variable;
+            }
+        }
+        public static DateTime GetCurrentDate()
+        {
+            return DateTime.Now;
+        }
+        
+        public int ReturnUserAge()
+        {
+            int UserAge = CurrentYear - _yearOfBirth;
+            if (_dayOfBith > CurrentDay && _monthOfBirth >= CurrentMonth)
+            {
+                UserAge--;
+            }
+            return UserAge;
+        }
+
+        public void ReadAllData()
+        {
+            Console.WriteLine($"Cześć {_name}! Urodziłeś się w {_placeOfLiving}. Masz {ReturnUserAge()} lat!");
+        }
+    }
+}
